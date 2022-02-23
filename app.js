993091dd62bc1df77ec329c636b560b280e5c40e
flkoliv <olivier@fleck.xyz>
2022-02-23 18:40:34 +0100

    ajout token

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+app.use(express.json());
 
+const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://piiquante:piiquante01@piiquante.i1xze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://piiquante:piiquante01@piiquante.i1xze.mongodb.net/piiquante?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -11,8 +13,14 @@ mongoose.connect('mongodb+srv://piiquante:piiquante01@piiquante.i1xze.mongodb.ne
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
-const userRoutes = require('./routes/user');
 app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
